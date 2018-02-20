883bd51852d9741eed90a0745af967a685973ab4
nnugent <nrnugent96@gmail.com>
2018-02-20 09:26:38 -0600

    Fillxed out outline for the ComputerPlayer

--- RockPaperScissors/ComputerPlayer.cs
+++ RockPaperScissors/ComputerPlayer.cs
@@ -6,5 +6,19 @@ namespace RockPaperScissors
 {
     class ComputerPlayer : Player
     {
+        List<string> nameOptions = new List<string> { "The World Destroyer", "Sheldon Cooper", "Darth Vader", "Obi-Wan Kenobi", "WunSwolBoi", "AyeNoDeWea"};
+        List<string> moveOptions = new List<string> { "Rock", "Paper", "Scissors", "Lizard", "Spock" };
+        Random rng = new Random();
+
+        public ComputerPlayer()
+        {
+            name = nameOptions[rng.Next(6)];
+        }
+
+        public override string GetMove()
+        {
+            move = moveOptions[rng.Next(5)];
+            return move;
+        }
     }
 }
