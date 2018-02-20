9c6bc3a868b26d49efdf1e673f693b8e2ba8fd95
nnugent <nrnugent96@gmail.com>
2018-02-20 08:30:17 -0600

    Add project files.

---
+++ RockPaperScissors/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockPaperScissors
+{
+    public abstract class Player
+    {
+        public string move;
+        public string name;
+        public int wins;
+
+        public virtual string GetMove()
+        {
+            return "";
+        }
+    }
+}
