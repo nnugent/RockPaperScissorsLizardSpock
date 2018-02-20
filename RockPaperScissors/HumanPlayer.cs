682fe55bd54f2fed983c5ccbd3781cba865deed4
nnugent <nrnugent96@gmail.com>
2018-02-20 09:14:02 -0600

    Filled out outline for the HumanPlayer.

--- RockPaperScissors/HumanPlayer.cs
+++ RockPaperScissors/HumanPlayer.cs
@@ -8,7 +8,15 @@ namespace RockPaperScissors
     {
         public HumanPlayer()
         {
-
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            wins = 0;
+        }
+        public override string GetMove()
+        {
+            Console.WriteLine("Rock, Paper, Scissors, Lizard, or Spock?");
+            move = Console.ReadLine();
+            return move;
         }
     }
 }
